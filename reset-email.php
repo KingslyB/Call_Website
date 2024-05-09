7338e588178405e7d515d6e27b1b39c26a922af2
Kingsly <kingsly.bude@gmail.com>
2024-05-09 19:23:49 -0400

    Added email reset

---
+++ reset-email.php
@@ -0,0 +1,44 @@
+<?php
+$title = "Email Reset";
+include("./includes/header.php");
+
+echo(uniqid("test"));
+
+if(!isset($_SESSION["id"])){
+    header("Location: sign-in.php");
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = trim($_POST["new-email"]);
+    $confirmEmail = trim($_POST["confirmation-email"]);
+
+    if(validateNewEmail($email, $confirmEmail, $_SESSION["id"])){
+        header("Location: sign-out.php");
+    }
+}
+
+if (isset($_SESSION["errorList"])){
+    echo('<div class="error-box visible-border"> <ul>');
+    foreach($_SESSION["errorList"] as $error){
+        echo('<li>'.$error.'</li>');
+    }
+    echo('</ul></div>');
+    unset($_SESSION["errorList"]);
+}
+?>
+
+
+
+<form method="post">
+    <h1 class="h3 mb-3 font-weight-normal">New Email Address</h1>
+    Current Email: <input type="text" name="new-email" value="<?php echo($_SESSION["emailaddress"])?>" disabled>
+    New Email: <input type="email" name="new-email" placeholder="New Email Address">
+    Confirmation Email: <input type="email" name="confirmation-email" placeholder="New Email Address">
+    <button class="btn btn-lg btn-primary btn-block mt-2" type="submit">Apply</button>
+</form>
+
+<a href="index.php"><button class="btn btn-lg btn-warning btn-block">Cancel</button></a>
+
+<?php
+include("./includes/footer.php");
+?>
