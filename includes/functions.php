fd06134cafbb58bd719ebae3c395f57c835fc37d
Kingsly <kingsly.bude@gmail.com>
2024-06-13 15:31:23 -0400

    Comments

--- includes/functions.php
+++ includes/functions.php
@@ -1,17 +1,27 @@
 <?php
 require_once 'mail.php';
 
+/**
+ * @return false|resource
+ * Database connection
+ */
 function db_connect(){
-    $CONNECTION = pg_connect("host=".DB_HOST.
+    return pg_connect("host=".DB_HOST.
         " port=" .DB_PORT.
         " dbname=" .DB_NAME.
         " user=" .DB_ADMIN_LOGIN.
         " password=" .DB_ADMIN_PASSWORD);
-    return $CONNECTION;
 }
 
-
-function attemptLogin($emailAddress, $password){
+/**
+ * @param string $emailAddress Client's email address
+ * @param string $password Client's plaintext password
+ * @return bool boolean depends on if the DB query was successful.
+ * @throws \Random\RandomException
+ * Find a user depending on the given credentials.
+ * If a user is found, the session is reset with account details and an auth "token" is saved to the DB.
+ */
+function attemptLogin(string $emailAddress, string $password) : bool{
     $_SESSION["errorList"] = array();
 
     if(!loginAuthenticate($emailAddress, $password)){
@@ -24,6 +34,10 @@ function attemptLogin($emailAddress, $password){
     return true;
 }
 
+/**
+ * @return void
+ * Immediately clears the client's auth cookie and session data
+ */
 function logout(){
     setcookie("a_cookie",
         "",
@@ -36,7 +50,14 @@ function logout(){
     clearSession();
 }
 
-function generateAuthSessionCookie($rememberMe){
+/**
+ * @param bool $rememberMe
+ * @return void
+ * @throws \Random\RandomException
+ * Generates an auth cookie that is stored in the DB
+ */
+//TODO: Implement "remember me" checkbox on sign-in.php page
+function generateAuthSessionCookie(bool $rememberMe){
     $token = bin2hex(random_bytes(16));
 
     setcookie("a_cookie",
@@ -50,7 +71,10 @@ function generateAuthSessionCookie($rememberMe){
     storeToken($token, $_SESSION['id']);
 }
 
-function UpdateAuth(){
+/**
+ * @return bool Returns true if a valid auth token was found in the DB and user data related to that token was updated in the session storage.
+ */
+function UpdateAuth() : bool{
     if(isset($_COOKIE["a_cookie"])){
         $updatedInfo = AuthCheck($_COOKIE["a_cookie"]);
 
@@ -59,9 +83,19 @@ function UpdateAuth(){
             return true;
         }
     }
+    return false;
 }
 
-function validateNewPassword($oldPassword, $newPassword, $confirmPassword, $userSessionID){
+/**
+ * @param $oldPassword User's old plaintext password
+ * @param $newPassword User's new plaintext password
+ * @param $confirmPassword User's new plaintext password again
+ * @param $userSessionID User's ID that is retrieved from the session storage.
+ * @return bool Returns true if ALL validation was successful AND the password was successfully changed in the DB
+ * Validate the arguments against password requirements.
+ * If they are ALL valid then the user specified by the logged-in user's session id has its password updated.
+ */
+function validateNewPassword(string $oldPassword, string $newPassword, string $confirmPassword, int $userSessionID) : bool{
     $_SESSION["errorList"] = array();
 
     if(strcmp($newPassword, $confirmPassword) != 0){
@@ -87,9 +121,11 @@ function validateNewPassword($oldPassword, $newPassword, $confirmPassword, $user
 }
 
 /**
- * Sets data from an associative array into the current session
+ * @param array $data Incoming session data.
+ * @return bool Returns false if $data is not an array or if any of the keys do not contain a string.
+ * Sets data from an  array into the current session
  */
-function addSessionData($data){
+function addSessionData(array $data) : bool{
     #return false if $data is not an array
     if(!is_array($data)){
         return false;
@@ -108,6 +144,10 @@ function addSessionData($data){
     return true;
 }
 
+/**
+ * @return void
+ * Clears the current session and starts a new one immediately
+ */
 function clearSession(){
     session_unset();
     session_destroy();
@@ -117,7 +157,15 @@ function clearSession(){
     ob_start();
 }
 
-function validateNewEmail($newEmailAddress, $confirmEmail, $userID) :bool
+/**
+ * @param string $newEmailAddress User's new email address again.
+ * @param string $confirmEmail User's new email address again.
+ * @param int $userID ID from the logged-in session.
+ * @return bool Returns true if ALL validation was successful AND the email address was successfully changed in the DB.
+ * Validate the arguments against email requirements.
+ * If they are ALL valid then the user specified by the logged-in user's session id has its email address updated.
+ */
+function validateNewEmail(string $newEmailAddress, string $confirmEmail, int $userID): bool
 {
     $_SESSION["errorList"] = array();
     if(strlen($newEmailAddress) < 6 || strlen($newEmailAddress) > 100){
@@ -135,7 +183,17 @@ function validateNewEmail($newEmailAddress, $confirmEmail, $userID) :bool
     return false;
 }
 
-function attemptRegistration($email, $password, $confirmPassword, $firstName, $lastName): bool{
+/**
+ * @param string $email New user's email address.
+ * @param string $password New user's plaintext password.
+ * @param string $confirmPassword New user's plaintext password again.
+ * @param string $firstName New user's first name.
+ * @param string $lastName New user's last name.
+ * @return bool Returns true if ALL validation was successful AND the new user was successfully added to the DB.
+ * Validate the arguments for creating a new user.
+ * If they are ALL valid then a new user will be created based on the information from the form.
+ */
+function attemptRegistration(string $email, string $password, string $confirmPassword, string $firstName, string $lastName): bool{
     $_SESSION["errorList"] = array();
 
     if(strlen($email) < 6 || strlen($email) > 100){
@@ -204,6 +262,7 @@ function displayTableData($columnHead, $columnValue){
             break;
     }
 }
+
 
 function preparePasswordReset($emailAddress){
     $newResetExists = newResetAttempt($emailAddress);
